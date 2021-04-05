596aa642bec37ca4fff474ba0bb09074aae36d72
Gulnaz.Zagrtdinova <gulnaz.zagrtdinova@tel-int.ru>
2021-04-05 16:51:27 +0300

    load menu from server

--- LearningExperience/Core/LearningExperience.Core.Documents/Models/DocumentsScheme.cs
+++ LearningExperience/Core/LearningExperience.Core.Documents/Models/DocumentsScheme.cs
@@ -1,14 +1,14 @@
-﻿namespace LearningExperience.Web.Blazor.Models
-{
-    using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
+namespace LearningExperience.Core.Documents.Models
+{
     public class DocumentsScheme
     {
         public List<Document> Documents { get; set; }
     }
 
     // ReSharper disable once StyleCop.SA1402
-    public class Document : Clickable
+    public class Document
     {
         public string Value { get; set; }
 
